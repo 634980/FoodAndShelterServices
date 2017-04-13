368cad6bc8b36564746a691328468f294cdb0196
634980 <Shwetha.Acharya@calbaptist.edu>
2017-04-13 01:37:33 +0000

    Impplementing auto complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     employee = Employee.find_by(username: params[:session][:username].downcase)
+    autocomplete :employee, :username
     if employee && employee.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in employee
